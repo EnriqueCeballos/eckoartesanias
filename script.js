c8a637c9950237c84d1b85b62b98da0c8a20e794
Enrique Ceballos <enrique_cpl12@hotmail.com>
2021-11-01 09:28:33 -0300

    Entrega Js 1

---
+++ script.js
@@ -0,0 +1,13 @@
+
+// VARIABLES
+let num1 = parseInt(prompt("Ingrese un número"));
+let num2 = parseInt(prompt("Ingrese un número"));
+let num3 = parseInt(prompt("Ingrese un número"));
+let num4 = parseInt(prompt("Ingrese un número"));
+
+// OPERACIONES
+let promedio =  (num1 + num2 + num3 + num4) / 4;
+
+console.log(promedio);
+
+alert(promedio);
